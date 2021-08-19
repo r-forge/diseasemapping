0b8200cfd587e51dc96b9677675d7850fdd1b335
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2021-08-19 15:35:15 +0000

    fill param bug fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2763 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/maternParamsGpu.R
+++ pkg/gpuRandom/R/maternParamsGpu.R
@@ -9,8 +9,8 @@ maternGpuParam = function(x, type='double') {
   x = geostatsp::fillParam(x)
   paramsGpu = vclMatrix(cbind(x, 
                                 matrix(0, nrow(x), 22-ncol(x))),type=type)
+  colnames(paramsGpu) = colnames(x)
   paramsGpu
-
   
 }
 
