af2c6ae0a4e0dd83f487ece8026b4a87de3193ca
ruoyong (Ruoyong Xu) <ruoyong.xu@mail.utoronto.ca>
2021-09-03 14:39:45 +0000

    maternParamsGpu updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2784 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/maternParamsGpu.R
+++ pkg/gpuRandom/R/maternParamsGpu.R
@@ -6,13 +6,49 @@
 
 
 maternGpuParam = function(x, type='double') {
+
+  
+  if (dim(x)[1]==1){
+  names<-colnames(x)
   x = geostatsp::fillParam(x)
-  paramsGpu = vclMatrix(cbind(x, 
-                                matrix(0, nrow(x), 22-ncol(x))),type=type)
+  x=matrix(x, nrow=1, ncol=length(x))
+  paramsGpu = vclMatrix(cbind(x,
+                              matrix(0, nrow(x), 22-ncol(x))),type=type)
+  gpuR::colnames(paramsGpu) = c(names,"anisoAngleDegrees")
+   }
+
+
+  else if(dim(x)[1]>1){
+  x = geostatsp::fillParam(x)
+  paramsGpu = vclMatrix(cbind(x, matrix(0, nrow(x), 22-ncol(x))),type=type)
   gpuR::colnames(paramsGpu) = colnames(x)
+  }
+  
   paramsGpu
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
