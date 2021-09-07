87101df16ebaa54102315dbb6385797db1985e54
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-09-07 14:54:34 +0000

    extract diagonals from ssqYX in likfitGpu finished
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2797 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gpuRandom/src/extractDiag.cpp
@@ -0,0 +1,188 @@
+#include "gpuRandom.hpp"
+#define DEBUGKERNEL
+template <typename T> 
+std::string extract_some_diag_string(int Ndatasets,    // number of Y's
+                                      int NpadColY, 
+                                      int NpadColYX,
+                                      int NpadBetweenMatricesYX) { 
+  
+  std::string typeString = openclTypeString<T>();
+  
+  
+  std::string result = "";
+  
+  if(typeString == "double") {
+    result += "\n#pragma OPENCL EXTENSION cl_khr_fp64 : enable\n";
+  }
+  
+  
+  result += 
+    //"#define NthisIteration " + std::to_string(NthisIteration) + "\n"
+    "#define Ndatasets " + std::to_string(Ndatasets) + "\n"    
+    "#define NpadColY " + std::to_string(NpadColY) + "\n"
+    "#define NpadBetweenMatricesYX "   + std::to_string(NpadBetweenMatricesYX) + "\n"
+    "#define NpadColYX "   + std::to_string(NpadColYX) + "\n";
+  
+  result += 
+    "\n__kernel void extract_some_diag(\n"
+    "__global " + typeString + " *ssqY,\n"
+    "__global " + typeString + " *ssqYX,\n"  
+    " int Nrowstart,\n"
+    " int NthisIteration ){\n\n";
+  
+  result +=
+    "int Drow, Dcol;\n";
+  
+  result += 
+    
+    "for (Drow = get_global_id(0); Drow < NthisIteration; Drow += get_global_size(0)) {\n"
+    "for (Dcol = get_global_id(1); Dcol < Ndatasets; Dcol += get_global_size(1)) {\n" 
+    
+    "ssqY[(Nrowstart+Drow) * NpadColY+Dcol] = ssqYX[ Drow*NpadBetweenMatricesYX + Dcol* NpadColYX + Dcol ];\n"
+    
+    "}\n"
+    "}\n"
+    "}\n";
+  
+  return(result);
+}
+
+
+
+
+
+
+
+
+
+
+
+void extractDiag(
+    viennacl::matrix<double> &ssqYX,// must be batches of square matrices
+    viennacl::matrix<double> &ssqY,
+    Rcpp::IntegerVector NparamPerIter,
+    int Ndatasets,
+    Rcpp::IntegerVector numWorkItems,
+    int ctx_id) {
+  
+  
+  
+  std::string extractSomeDiagKernelString = extract_some_diag_string<double>(//NparamPerIter[0],  
+    Ndatasets,
+    ssqY.internal_size2(), 
+    ssqYX.internal_size2(), 
+    ssqYX.internal_size2()*ssqYX.size2());
+  
+  
+#ifdef DEBUGKERNEL
+  Rcpp::Rcout << extractSomeDiagKernelString << "\n\n";
+#endif    
+  
+  
+  
+  // the context
+  viennacl::ocl::context ctx(viennacl::ocl::get_context(ctx_id));
+  viennacl::ocl::program & my_prog = ctx.add_program(extractSomeDiagKernelString, "my_kernel");
+  
+  
+  viennacl::ocl::kernel &extractSomeDiagKernel = my_prog.get_kernel("extract_some_diag");
+  extractSomeDiagKernel.global_work_size(0, numWorkItems[0]);
+  extractSomeDiagKernel.global_work_size(1, numWorkItems[1]);
+  
+  extractSomeDiagKernel.local_work_size(0, 1L);
+  extractSomeDiagKernel.local_work_size(1, 1L);
+  
+  int Nparams = ssqYX.size1()/ssqYX.size2();
+  int Niter = ceil( ( (double) Nparams) / ((double) NparamPerIter[0]));
+  int Diter;
+  int endThisIteration;
+  int DiterIndex, NthisIteration;
+  
+  //viennacl::ocl::command_queue theQueue = extractSomeDiagKernel.context().get_queue();
+  for (Diter=0, DiterIndex=0; 
+       Diter< Niter; 
+       Diter++, DiterIndex += NparamPerIter[0]){
+    
+    endThisIteration = std::min(DiterIndex + NparamPerIter[0], Nparams);
+    NthisIteration = endThisIteration - DiterIndex;
+    
+    // if(verbose[0]) {
+    //   Rcpp::Rcout << "\n" << "Diter " << Diter <<" DiterIndex " << DiterIndex << " endThisIteration " << 
+    //     endThisIteration << " Nthisiteration " << NthisIteration  << "\n";
+    // }
+    
+    
+    viennacl::ocl::enqueue(extractSomeDiagKernel(ssqY, ssqYX, DiterIndex, NthisIteration));     //Rcpp::Rcout << "after enqueue" << "\n\n";
+    
+    
+  } // Diter
+  
+  
+  //clFinish(theQueue.handle().get());
+  
+  
+  
+}
+
+
+
+//template<typename T> 
+void extractDiagTemplated(
+    Rcpp::S4 ssqYXR,// viennacl::vector_base<int>  rowSum, viennacl::vector_base<int>  colSum,  
+    Rcpp::S4 ssqYR,
+    Rcpp::IntegerVector NparamPerIter,
+    int Ndatasets,
+    Rcpp::IntegerVector numWorkItems) {
+  
+  
+  const bool BisVCL=1;
+  const int ctx_id = INTEGER(ssqYXR.slot(".context_index"))[0]-1;
+  std::shared_ptr<viennacl::matrix<double> > ssqYX = getVCLptr<double>(ssqYXR.slot("address"), BisVCL, ctx_id);
+  std::shared_ptr<viennacl::matrix<double> > ssqY = getVCLptr<double>(ssqYR.slot("address"), BisVCL, ctx_id);
+  
+  extractDiag(
+    *ssqYX,// viennacl::vector_base<int>  rowSum, viennacl::vector_base<int>  colSum,  
+    *ssqY,
+    NparamPerIter,
+    Ndatasets,
+    numWorkItems,
+    ctx_id);
+  
+  
+}
+
+
+
+
+
+
+// [[Rcpp::export]]
+void extractDiagBackend(
+    Rcpp::S4 ssqYXR,// viennacl::vector_base<int>  rowSum, viennacl::vector_base<int>  colSum,  
+    Rcpp::S4 ssqYR,
+    Rcpp::IntegerVector NparamPerIter,
+    int Ndatasets,
+    Rcpp::IntegerVector numWorkItems) {
+  
+  
+  extractDiagTemplated(ssqYXR,ssqYR, NparamPerIter,Ndatasets,numWorkItems);
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
