14f99e9c7bb0794ddf9e7a5ebc610131f0877241
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-10-10 15:03:40 +0000

    new examples in vignnettes, weird problems
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2902 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/src/crossprodBatch.cpp
+++ pkg/gpuRandom/src/crossprodBatch.cpp
@@ -497,15 +497,6 @@ SEXP crossprodBatchTyped(
 //' Multiply crossproduct matrices
 //' 
 //' Computes C = t(A) D A
-//'
-//' @param C output matrices, stacked row-wise
-//' @param A rectangular matrices
-//' @param D rectangular matrix, columns are diagonals
-//' @param invertD set to 1 for C = t(A) D^(-1) A
-//' @param Nglobal vector of number of global work items
-//' @param Nlocal vector of number of local work items
-//' @param NlocalCache elements in local cache
-//' @export
 // [[Rcpp::export]]
 SEXP crossprodBatchBackend(
     Rcpp::S4 C,
