a24cecd703fa1a5c39a1287f4d57c2fb90d9e054
ruoyong (Ruoyong Xu) <ruoyong.xu@mail.utoronto.ca>
2021-11-24 19:19:32 +0000

    new profile fun for betascalar
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2997 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/likLgm_betascalarProfile.R
+++ pkg/gpuRandom/R/likLgm_betascalarProfile.R
@@ -86,4 +86,93 @@ likLgm_betascalarProfile <- function(Betas, #a m x 1 R vector  given by the user
   
   
   
-  
+# likLgm_betascalarProfile <- function(Betas, #a m x 1 R vector  given by the user 
+#                                      a,     # which beta_i?
+#                                      Nobs,  # number of observations.
+#                                      Ndata,
+#                                      Nparam,
+#                                      Ncov,
+#                                      detVar, # vclVector
+#                                      ssqY,   # vclMatrix
+#                                      XVYXVX,   # vclMatrix
+#                                      jacobian, # vclVector  #form = c("loglik", "profileforBeta"),
+#                                      minustwotimes=TRUE){
+#   
+#   
+#   ssqY <- as.matrix(ssqY)
+#   detVar <- as.vector(detVar)
+#   detVar <- matrix(rep(detVar, Ndata), nrow=Nparam)
+#   jacobian <- as.vector(jacobian)
+#   jacobian <- do.call(rbind, replicate(Nparam, jacobian, simplify = FALSE))
+#   #dim(XVYXVX)
+#   Ucov <- Ncov-1
+#   m <- length(Betas)
+#   
+#   XTVinvX <- XVYXVX[ , (ncol(XVYXVX)-Ncov+1):ncol(XVYXVX)]
+#   XVY <- XVYXVX[ , 1:Ndata]
+#   
+#   ## make each symmetric
+#   # for (i in 1:Nparam){
+#   #   #mat <- XTVinvX[((i-1)*Ncov+1) : (i*Ncov), ]
+#   #   XTVinvX[((i-1)*Ncov+1) : (i*Ncov), ][upper.tri(XTVinvX[((i-1)*Ncov+1) : (i*Ncov), ])] <- XTVinvX[((i-1)*Ncov+1) : (i*Ncov), ][lower.tri(XTVinvX[((i-1)*Ncov+1) : (i*Ncov), ])]
+#   # }
+#   
+#   
+#   selectedrows <- seq_len(Nparam)*a
+#   XTVinvX_deleted <- matrix(XTVinvX[-selectedrows,-a],ncol=Ucov, byrow=TRUE)
+#   XTVinvX_a <- matrix(XTVinvX[selectedrows, -a], nrow=Nparam, ncol=Ucov)
+#   #XTVinvX_a <- matrix(XTVinvX[-selectedrows, a], nrow=Nparam*Ucov, ncol=1)[1:10,]
+#   XVY_deleted <- XVY[-selectedrows, ]
+#   X_aVY <- XVY[selectedrows, ]
+#   X_aVX_a <- XTVinvX[selectedrows, a]
+#   #dim(XVY_deleted)
+#   
+#   
+#   partA = matrix(0, nrow=Nparam, ncol=Ndata)
+#   partB = matrix(0, nrow=Nparam, ncol=Ndata)
+#   partC = matrix(0, nrow=Nparam, ncol=Ndata)
+#   partD = matrix(0, nrow=Nparam, ncol=Ndata)
+#   partE = matrix(0, nrow=Nparam, ncol=Ndata)
+#   LogLik_optimized = matrix(0, nrow=m, ncol=1)
+#   
+#   
+#   
+#   
+#   i_vec <- seq_len(Nparam)
+#   computed <- parallel::mcmapply(i_vec,  
+#                                  FUN = function(i) {
+#                                    interval <- c(((i-1)*Ucov+1) : (i*Ucov))
+#                                    solve(XTVinvX_deleted[interval, ]) 
+#                                  }
+#   )
+#   
+#   computed = matrix(computed, ncol = Nparam)
+#   
+#   for (i in 1:Nparam){
+#     interval <- c(((i-1)*Ucov+1) : (i*Ucov))
+#     temp <- matrix(computed[,i], ncol = Ucov)
+#     
+#     # part (A) have 2 data sets
+#     partA[i,] = rowSums(XVY_deleted[interval,] %*% temp) * XVY_deleted[interval,]
+#     # part (B) have 2 data sets.   has beta
+#     partB[i,] = -2*XVY_deleted[interval,] %*% temp %*% XTVinvX_a[i,]
+#     # part (C) no data sets.  has beta
+#     partC[i,] = XTVinvX_a[i, ] %*% temp %*% XTVinvX_a[i, ]
+#     #partC[i,] = XTVinvX_a[interval, ] %*% temp %*% XTVinvX_a[interval, ]
+#     # part (D) have 2 data sets.    has beta
+#     partD[i,] = -2* X_aVY[i, ]
+#     # part (E)
+#     partE[i,] = X_aVX_a[i]
+#   }
+#   
+#   
+#   for (bet in 1:m){
+#     ssqResidual <- ssqY + Betas[bet] *partD + Betas[bet]^2 *partE - (partA + Betas[bet]* partB + Betas[bet]^2 * partC)
+#     All_min2loglik_forthisbeta <- Nobs*log(ssqResidual/Nobs) + detVar + Nobs + Nobs*log(2*pi) + jacobian
+#     LogLik_optimized[bet,] = min(All_min2loglik_forthisbeta)
+#   }
+#   
+#   
+#   LogLik_optimized
+#   
+# }  
