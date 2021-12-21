e0470ffbd9893f549b6659770e22a948ab7527ba
ruoyong (Ruoyong Xu) <ruoyong.xu@mail.utoronto.ca>
2021-12-21 17:02:50 +0000

    add 2d r fun
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3052 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/prof2Dcov.R
+++ pkg/gpuRandom/R/prof2Dcov.R
@@ -25,17 +25,30 @@
           maximum <- max(LogLik)
           breaks = maximum - qchisq(cilevel,  df = 2)/2
          
-          result = as.data.table(cbind(LogLik, params[ ,paramToEstimate]))
+          index <- which(LogLik == max(LogLik, na.rm = TRUE), arr.ind = TRUE)
+
+          makedata <- data.frame(cbind(params[,paramToEstimate], LogLik[,index[2]]))
+          colnames(makedata) <- c(paramToEstimate, 'LogLik')
+
+          lowx = min(params[,paramToEstimate[1]])
+          uppx = max(params[,paramToEstimate[1]])
+          lowy = min(params[,paramToEstimate[2]])
+          uppy = max(params[,paramToEstimate[2]])
 
 
-          profileLogLik2D <- result[, .(profile=max(.SD)), by=paramToEstimate]
-          profileLogLik2D <- as.matrix(profileLogLik2D)
-          
-          lMatrix = matrix(profileLogLik2D[,3], length(ParamList[[1]]), length(ParamList[[2]]))
-          contour(ParamList[[1]]/100, ParamList[[2]], lMatrix,
-                  col = par("fg"), lty = par("lty"), lwd = par("lwd"),
-                  add = FALSE, levels = breaks)
-          points(x = params[,paramToEstimate[1]]/100, y = params[,paramToEstimate[2]], pch=20, col='grey', cex=0.5)
+          contourplot <- ggplot(makedata, aes_string(x = paramToEstimate[1], y = paramToEstimate[2], z='LogLik')) + theme_bw() +
+            scale_y_continuous(limits=c(lowy, uppy))+
+            scale_x_continuous(limits=c(lowx, uppx) )+
+            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+            geom_point(size = 0.5, colour='grey') +
+            stat_contour(breaks=breaks)
+
+
+
+
+          contourplot
+
+
           
         }
         
@@ -69,4 +82,30 @@
         # 
         # 
         # 
-        # contourplot        
+        # contourplot    
+        
+        
+        
+        
+        
+        # result = as.data.table(cbind(LogLik, params[ ,paramToEstimate]))
+        # 
+        # 
+        # profileLogLik2D <- result[, .(profile=max(.SD)), by=paramToEstimate]
+        # profileLogLik2D <- as.matrix(profileLogLik2D)
+        # 
+        # lMatrix = matrix(profileLogLik2D[,3], length(ParamList[[1]]), length(ParamList[[2]]))
+        # contour(ParamList[[1]]/100, ParamList[[2]], lMatrix,
+        #         col = par("fg"), lty = par("lty"), lwd = par("lwd"),
+        #         add = FALSE, levels = breaks)
+        # points(x = params[,paramToEstimate[1]]/100, y = params[,paramToEstimate[2]], pch=20, col='grey', cex=0.5)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
