7925f2153bb4a61067a411948bb54795644a8945
ruoyong (Ruoyong Xu) <ruoyong.xu@mail.utoronto.ca>
2021-12-25 05:58:58 +0000

    streamscpu
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3068 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/src/streamsCpu.cpp
+++ pkg/gpuRandom/src/streamsCpu.cpp
@@ -8,180 +8,180 @@ using namespace Rcpp;
 
 
 /*
-//Uniform number kernel
-template <typename T> 
-std::string mrg31k3pTypeString() {
-  return("undefined");}
-
-template <> std::string mrg31k3pTypeString<double>(){
-  std::string result;
-  result = mrg31k3pDoubleUnifString;
-  return(result);
-}
-template <> std::string mrg31k3pTypeString<float>(){
-  std::string result;
-  result =  mrg31k3pFloatUnifString;
-  return(result);
-}
-template <> std::string mrg31k3pTypeString<int>(){
-  std::string result;
-  result = mrg31k3pIntegerUnifString;
-  return(result);
-}
-
-
-//Normal number kernel
-template <typename T> 
-std::string mrg31k3pNormString() {
-  return("undefined");}
-
-template <> std::string mrg31k3pNormString<double>(){
-  std::string result;
-  result = mrg31k3pDoubleNormString;
-  return(result);
-}
-template <> std::string mrg31k3pNormString<float>(){
-  std::string result;
-  result = mrg31k3pFloatNormString;
-  return(result);
-}
-template <> std::string mrg31k3pNormString<int>(){
-  return("undefined");
-}
-
-
-//////////////////////////////////the main function;//////////////////////
-template<typename T>
-void gpuRn(
-    viennacl::vector_base<T> &x, 
-    Rcpp::IntegerMatrix streamsR, 
-    IntegerVector numWorkItems,
-    IntegerVector numLocalItems,
-    int ctx_id,
-    std::string  random_type){
-  
-  viennacl::ocl::context ctx(viennacl::ocl::get_context(ctx_id));
-  std::string mrg31k3pkernelString;
-  
-  if(random_type == "uniform"){
-    mrg31k3pkernelString = mrg31k3pTypeString<T>();
-  }  else if(random_type == "normal"){
-    mrg31k3pkernelString = mrg31k3pNormString<T>();
-  } else {
-    Rcout << random_type << "\n";
-    warning("random_type undefined");
-  }
-  
-  // create streams
-  size_t streamBufferSize;   
-  clrngStatus err;
-  
-  //Reserve memory space for count stream objects, without creating the stream objects. 
-  //Returns a pointer to the newly allocated buffer. 
-  clrngMrg31k3pStream* streams = clrngMrg31k3pAllocStreams(numWorkItems[0]*numWorkItems[1], &streamBufferSize, &err);
-  //count	Number of stream objects to allocate.
-  
-  
-  // transfer streams to opencl as matrix
-  // convert to crngMgr31k3pStream in opencl, but still on host
-  convertMatclRng(streamsR, streams);
-  
-  
-  // Create buffer to transfer streams to the device.
-  viennacl::vector<char> bufIn(ctx.create_memory_without_smart_handle( 
-      CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,  streamBufferSize, (void *) streams), 1);
-  
-  
-  // kernel, in the kernel will Copy RNG host stream objects from global memory into private memory
-  viennacl::ocl::program &my_prog = ctx.add_program(mrg31k3pkernelString, "my_kernel");
-  
-  viennacl::ocl::kernel &random_number = my_prog.get_kernel("mrg31k3p");
-  
-  random_number.global_work_size(0, numWorkItems[0]);
-  random_number.global_work_size(1, numWorkItems[1]);
-  
-  random_number.local_work_size(0, numLocalItems[0]);
-  random_number.local_work_size(1, numLocalItems[1]);
-  
-  int Nsim = x.size();
-  viennacl::ocl::enqueue(random_number(bufIn, x, Nsim) ); //streams, out, vector_size
-  
-  
-  // copy streams back to cpu
-  viennacl::backend::memory_read(bufIn.handle(), 0, streamBufferSize, streams);
-  
-  // then transfer to R object, //return streams to R 
-  convertclRngMat(streams, streamsR);
-  
-}
-
-
-
-
-
-
-///////////////////////////////// //////////
-template<typename T> 
-SEXP gpuRn(
-    Rcpp::S4  xR,   
-    Rcpp::IntegerMatrix streamsR,  
-    IntegerVector max_global_size,     
-    IntegerVector max_local_size,
-    std::string  random_type) 
-{
-  // data
-  const bool BisVCL=1;
-  const int ctx_id = INTEGER(xR.slot(".context_index"))[0]-1;
-  
-  std::shared_ptr<viennacl::vector_base<T> > x = getVCLVecptr<T>(xR.slot("address"), BisVCL, ctx_id);
-  
-  gpuRn<T>(*x, streamsR, max_global_size, max_local_size, ctx_id, random_type);
-  
-  return(Rcpp::wrap(1L));	
-}
-
-
-
-
-//[[Rcpp::export]]
-SEXP cpp_gpuRn(
-    Rcpp::S4  xR,  
-    Rcpp::IntegerMatrix streamsR,   
-    IntegerVector max_global_size,     
-    IntegerVector max_local_size,
-    std::string random_type,
-    std::string precision_type) 
-{
-  if(precision_type == "float") {
-    return gpuRn<float>(xR, streamsR, max_global_size, max_local_size,random_type);
-  } else if (precision_type=="double") {
-    return gpuRn<double>(xR, streamsR, max_global_size, max_local_size,random_type);
-  } else {
-    return gpuRn<int>(xR, streamsR, max_global_size, max_local_size,random_type);
-  }
-}
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-void runifGpuHost(viennacl::vector_base<double> &x)//use them to generate numbers on the host
-{
-  
-  int D, N=x.size();
-  clrngMrg31k3pStream* stream = clrngMrg31k3pCreateStreams(NULL, 1, NULL, NULL);
-  
-  for(D =0; D<N; D++) {
-    x(D) = clrngMrg31k3pRandomU01(stream);
-  }
-}
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-*/
+ //Uniform number kernel
+ template <typename T> 
+ std::string mrg31k3pTypeString() {
+ return("undefined");}
+ 
+ template <> std::string mrg31k3pTypeString<double>(){
+ std::string result;
+ result = mrg31k3pDoubleUnifString;
+ return(result);
+ }
+ template <> std::string mrg31k3pTypeString<float>(){
+ std::string result;
+ result =  mrg31k3pFloatUnifString;
+ return(result);
+ }
+ template <> std::string mrg31k3pTypeString<int>(){
+ std::string result;
+ result = mrg31k3pIntegerUnifString;
+ return(result);
+ }
+ 
+ 
+ //Normal number kernel
+ template <typename T> 
+ std::string mrg31k3pNormString() {
+ return("undefined");}
+ 
+ template <> std::string mrg31k3pNormString<double>(){
+ std::string result;
+ result = mrg31k3pDoubleNormString;
+ return(result);
+ }
+ template <> std::string mrg31k3pNormString<float>(){
+ std::string result;
+ result = mrg31k3pFloatNormString;
+ return(result);
+ }
+ template <> std::string mrg31k3pNormString<int>(){
+ return("undefined");
+ }
+ 
+ 
+ //////////////////////////////////the main function;//////////////////////
+ template<typename T>
+ void gpuRn(
+ viennacl::vector_base<T> &x, 
+ Rcpp::IntegerMatrix streamsR, 
+ IntegerVector numWorkItems,
+ IntegerVector numLocalItems,
+ int ctx_id,
+ std::string  random_type){
+ 
+ viennacl::ocl::context ctx(viennacl::ocl::get_context(ctx_id));
+ std::string mrg31k3pkernelString;
+ 
+ if(random_type == "uniform"){
+ mrg31k3pkernelString = mrg31k3pTypeString<T>();
+ }  else if(random_type == "normal"){
+ mrg31k3pkernelString = mrg31k3pNormString<T>();
+ } else {
+ Rcout << random_type << "\n";
+ warning("random_type undefined");
+ }
+ 
+ // create streams
+ size_t streamBufferSize;   
+ clrngStatus err;
+ 
+ //Reserve memory space for count stream objects, without creating the stream objects. 
+ //Returns a pointer to the newly allocated buffer. 
+ clrngMrg31k3pStream* streams = clrngMrg31k3pAllocStreams(numWorkItems[0]*numWorkItems[1], &streamBufferSize, &err);
+ //count	Number of stream objects to allocate.
+ 
+ 
+ // transfer streams to opencl as matrix
+ // convert to crngMgr31k3pStream in opencl, but still on host
+ convertMatclRng(streamsR, streams);
+ 
+ 
+ // Create buffer to transfer streams to the device.
+ viennacl::vector<char> bufIn(ctx.create_memory_without_smart_handle( 
+ CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,  streamBufferSize, (void *) streams), 1);
+ 
+ 
+ // kernel, in the kernel will Copy RNG host stream objects from global memory into private memory
+ viennacl::ocl::program &my_prog = ctx.add_program(mrg31k3pkernelString, "my_kernel");
+ 
+ viennacl::ocl::kernel &random_number = my_prog.get_kernel("mrg31k3p");
+ 
+ random_number.global_work_size(0, numWorkItems[0]);
+ random_number.global_work_size(1, numWorkItems[1]);
+ 
+ random_number.local_work_size(0, numLocalItems[0]);
+ random_number.local_work_size(1, numLocalItems[1]);
+ 
+ int Nsim = x.size();
+ viennacl::ocl::enqueue(random_number(bufIn, x, Nsim) ); //streams, out, vector_size
+ 
+ 
+ // copy streams back to cpu
+ viennacl::backend::memory_read(bufIn.handle(), 0, streamBufferSize, streams);
+ 
+ // then transfer to R object, //return streams to R 
+ convertclRngMat(streams, streamsR);
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ ///////////////////////////////// //////////
+ template<typename T> 
+ SEXP gpuRn(
+ Rcpp::S4  xR,   
+ Rcpp::IntegerMatrix streamsR,  
+ IntegerVector max_global_size,     
+ IntegerVector max_local_size,
+ std::string  random_type) 
+ {
+ // data
+ const bool BisVCL=1;
+ const int ctx_id = INTEGER(xR.slot(".context_index"))[0]-1;
+ 
+ std::shared_ptr<viennacl::vector_base<T> > x = getVCLVecptr<T>(xR.slot("address"), BisVCL, ctx_id);
+ 
+ gpuRn<T>(*x, streamsR, max_global_size, max_local_size, ctx_id, random_type);
+ 
+ return(Rcpp::wrap(1L));	
+ }
+ 
+ 
+ 
+ 
+ //[[Rcpp::export]]
+ SEXP cpp_gpuRn(
+ Rcpp::S4  xR,  
+ Rcpp::IntegerMatrix streamsR,   
+ IntegerVector max_global_size,     
+ IntegerVector max_local_size,
+ std::string random_type,
+ std::string precision_type) 
+ {
+ if(precision_type == "float") {
+ return gpuRn<float>(xR, streamsR, max_global_size, max_local_size,random_type);
+ } else if (precision_type=="double") {
+ return gpuRn<double>(xR, streamsR, max_global_size, max_local_size,random_type);
+ } else {
+ return gpuRn<int>(xR, streamsR, max_global_size, max_local_size,random_type);
+ }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ////////////////////////////////////////////////////////////////////////////////////////////////
+ void runifGpuHost(viennacl::vector_base<double> &x)//use them to generate numbers on the host
+ {
+ 
+ int D, N=x.size();
+ clrngMrg31k3pStream* stream = clrngMrg31k3pCreateStreams(NULL, 1, NULL, NULL);
+ 
+ for(D =0; D<N; D++) {
+ x(D) = clrngMrg31k3pRandomU01(stream);
+ }
+ }
+ ////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ 
+ */
 
 
 
@@ -193,36 +193,36 @@ void runifGpuHost(viennacl::vector_base<double> &x)//use them to generate number
 
 
 /*
-//matrix ->clRNG streams
-void convertMatclRng(Rcpp::IntegerMatrix Sin, clrngMrg31k3pStream* streams){
-  
-  int Ditem,Delement,Dcis,Dg;
-  int numWorkItems = Sin.nrow();
-  
-  for(Ditem =0;Ditem < numWorkItems;Ditem++){
-    for(Delement=0;Delement < 3;Delement++){
-      
-      Dcis=0;
-      Dg=0;
-      streams[Ditem].current.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].current.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      
-      Dcis=1;
-      Dg=0;
-      streams[Ditem].initial.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].initial.g2[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      
-      Dcis=2;
-      Dg=0;
-      streams[Ditem].substream.g1[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].substream.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-    }
-  }
-}
-
+ //matrix ->clRNG streams
+ void convertMatclRng(Rcpp::IntegerMatrix Sin, clrngMrg31k3pStream* streams){
+ 
+ int Ditem,Delement,Dcis,Dg;
+ int numWorkItems = Sin.nrow();
+ 
+ for(Ditem =0;Ditem < numWorkItems;Ditem++){
+ for(Delement=0;Delement < 3;Delement++){
+ 
+ Dcis=0;
+ Dg=0;
+ streams[Ditem].current.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ Dg=1;
+ streams[Ditem].current.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ 
+ Dcis=1;
+ Dg=0;
+ streams[Ditem].initial.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ Dg=1;
+ streams[Ditem].initial.g2[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ 
+ Dcis=2;
+ Dg=0;
+ streams[Ditem].substream.g1[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ Dg=1;
+ streams[Ditem].substream.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
+ }
+ }
+ }
+ 
  */
 
 // clRNG -> Matrix
@@ -250,7 +250,7 @@ void convertclRngMat(clrngMrg31k3pStream* streams, Rcpp::IntegerMatrix result) {
       // result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].substream.g1[Delement];//0,12
       // Dg=1;
       // result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].substream.g2[Delement];//0,15
-  
+      
     }
   }
   
@@ -288,12 +288,12 @@ struct clrngMrg31k3pStreamCreator_ {
   cl_uint nuA1[3][3];
   cl_uint nuA2[3][3];
 };        
-static clrngMrg31k3pStreamCreator defaultStreamCreator = {
-  BASE_CREATOR_STATE,
-  BASE_CREATOR_STATE,
-  BASE_CREATOR_JUMP_MATRIX_1,
-  BASE_CREATOR_JUMP_MATRIX_2
-};
+// static clrngMrg31k3pStreamCreator defaultStreamCreator = {
+//   BASE_CREATOR_STATE,
+//   BASE_CREATOR_STATE,
+//   BASE_CREATOR_JUMP_MATRIX_1,
+//   BASE_CREATOR_JUMP_MATRIX_2
+// };
 
 
 // //[[Rcpp::export]]
@@ -331,12 +331,12 @@ clrngMrg31k3pStreamCreator buildStreamCreator(Rcpp::IntegerVector seed){
     BASE_CREATOR_JUMP_MATRIX_2
   };
   
-    for(Ditem = 0; Ditem < 3; Ditem++) {
-      streamCreatorHere.initialState.g1[Ditem] = seed[Ditem];
-      streamCreatorHere.initialState.g2[Ditem] = seed[Ditem+3];
-      streamCreatorHere.nextState.g1[Ditem] = seed[Ditem];
-      streamCreatorHere.nextState.g2[Ditem] = seed[Ditem+3];
-    }
+  for(Ditem = 0; Ditem < 3; Ditem++) {
+    streamCreatorHere.initialState.g1[Ditem] = seed[Ditem];
+    streamCreatorHere.initialState.g2[Ditem] = seed[Ditem+3];
+    streamCreatorHere.nextState.g1[Ditem] = seed[Ditem];
+    streamCreatorHere.nextState.g2[Ditem] = seed[Ditem+3];
+  }
   return streamCreatorHere;
 }
 
@@ -350,34 +350,31 @@ Rcpp::IntegerMatrix  createStreamsCpuBackend(
   
   //IntegerVector seed2 = rep_len(initial, 6);
   Rcpp::IntegerMatrix result=Rcpp::IntegerMatrix(n[0], 12L);
-   
+  
   colnames(result) = CharacterVector::create(
     "current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
     "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3");
   //  "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3");
-
+  
   size_t streamBufferSize;
   clrngStatus err;
-
-//  int numWorkItems =result.nrow();
+  
+  //  int numWorkItems =result.nrow();
   //  int Ditem,Delement,Dcis,Dg;
   
   clrngMrg31k3pStreamCreator streamCreatorHere = buildStreamCreator(initial);
   
   // Rcpp::Rcout << "a" << streamCreatorHere.initialState.g1[0]<< " " << streamCreatorHere.initialState.g1[1]<< " " << streamCreatorHere.initialState.g1[2]<< "\n";
   // Rcpp::Rcout << "b" << streamCreatorHere.initialState.g2[0]<< " " << streamCreatorHere.initialState.g2[1]<< " " << streamCreatorHere.initialState.g2[2]<< "\n";
-
+  
   clrngMrg31k3pStream* streams = clrngMrg31k3pCreateStreams(&streamCreatorHere, n[0], 
                                                             &streamBufferSize, &err);//line 299 in mrg31k3p.c
- 
-
+  
+  
   convertclRngMat(streams, result);
   
   return result;
 }
-
-
-
 
 
 
