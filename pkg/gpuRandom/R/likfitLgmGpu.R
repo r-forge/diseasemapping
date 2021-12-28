3b63bb81096d0f3db20abef704aea25164c1dc5e
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-12-28 18:41:57 +0000

    r functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3074 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/likfitLgmGpu.R
+++ pkg/gpuRandom/R/likfitLgmGpu.R
@@ -8,19 +8,15 @@
 
 # betahat and sigmahat 
 # profile log-likelihood for each covariance parameters + lambda
- likfitLgmGpu <- function(data,
+ likfitLgmCov2d <- function(data,
                          formula, 
                          coordinates,
                          params, # CPU matrix for now, users need to provide proper parameters given their specific need
-                         #fixVariance = FALSE, 
                          boxcox,  # boxcox is always estimated
-                         paramToEstimate = c("range", "shape", "nugget", "anisoRatio", "anisoAngleDegrees", "boxcox"), #variance and regression parameters are always estimated if not given,
                          cilevel=0.95,  # decimal
                          type = c("float", "double")[1+gpuInfo()$double_support],
                          reml=FALSE, 
                          NparamPerIter,
-                         df,
-                         minustwotimes=FALSE,
                          Nglobal,
                          Nlocal,
                          NlocalCache,
@@ -29,11 +25,7 @@
   # will always be c(1,0,.......)
   boxcox = c(1, 0, setdiff(boxcox, c(0,1)))
   
-  if(length(boxcox)<=3 & is.element('boxcox',paramToEstimate))
-  {
-    warning("need more params to estimate boxcox")
-  }
-  
+
   
   
   # get rid of NAs in data
@@ -75,7 +67,6 @@
   ssqY <- vclMatrix(0, Nparam, Ndata, type=type)
   XVYXVX = vclMatrix(0, Nparam * Ncov, ncol(yx), type=type)
   ssqBetahat <- vclMatrix(0, Nparam, Ndata, type=type)
-  ssqBeta <- vclMatrix(0, Nparam, Ndata, type=type)
   detVar = vclVector(0, Nparam,type=type)
   detReml = vclVector(0, Nparam, type=type)
   jacobian = vclVector(0, Ndata, type=type)   
@@ -102,7 +93,7 @@
     as.integer(Nglobal),  #12
     as.integer(Nlocal),  #16
     NlocalCache,  #14
-    verbose=1,  #15
+    verbose=verbose,  #15
     ssqYX, #
     ssqYXcopy,  #new
     LinvYX,  #18
@@ -185,7 +176,7 @@
   
   LogLikcpu <- as.matrix(-0.5*minusTwoLogLik)
   maximum <- max(LogLikcpu)
-  breaks = maximum - qchisq(cilevel,  df = df)/2
+  breaks = maximum - qchisq(cilevel,  df = 2)/2
   
   # if(twoDbreak == TRUE){
   #   breaks2D = maximum - qchisq(cilevel,  df = 2)/2 
@@ -195,357 +186,43 @@
   
   
   ############## output matrix ####################
-  Table <- matrix(NA, nrow=length(union(paramToEstimate, 'boxcox'))+Ncov+1, ncol=3)
-  rownames(Table) <-  c("intercept", paste(c('betahat'), seq_len(Ncov-1),sep = ''), "sdSpatial", union(paramToEstimate, 'boxcox'))
-  colnames(Table) <-  c("estimate", paste(c('lower', 'upper'), cilevel*100, 'ci', sep = ''))
-  
-  
-
-  
-  ############### profile for covariance parameters #####################
-  if(is.element('range',paramToEstimate)){
-    # get profile log-lik for range values
-    result = data.table::as.data.table(cbind(LogLikcpu, params0[,"range"]))
-    colnames(result) <- c(paste(c('boxcox'), round(boxcox, digits = 2) ,sep = ''), "range")
-    profileLogLik <- result[, .(profile=max(.SD)), by=range]
-    f1 <- approxfun(profileLogLik$range, profileLogLik$profile-breaks)  
-    # f1 <- splinefun(profileLogLik$range, profileLogLik$profile-breaks, method = "monoH.FC")
-    # plot(profileLogLik$range, profileLogLik$profile-breaks)
-    # plot(profileLogLik$range, profileLogLik$profile)
-    # curve(f1(x), add = TRUE, col = 2, n = 1001)   #the number of x values at which to evaluate
-    # abline(h =0, lty = 2)
-    # rangeresults <- optim(26000, f1, method = "L-BFGS-B",lower = 20000, upper = 240000, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(profileLogLik$range)
-    upper = max(profileLogLik$range)
-    MLE <- optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum
-    # maxvalue <- rangeresults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci for range, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci for range, require more params")}
-    }
-    
-    if(length(ci)==0 | length(ci)>2){
-      warning("require a better param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["range",] <- c(MLE, ci)
-    # MLE <- profileLogLik$range[which.max(profileLogLik$profile)]
-    # plot(profileLogLik$range, profileLogLik$profile)
-    # f1<-approxfun(profileLogLik$range, profileLogLik$profile-breaks)  
-    # optimization <- optimize(f1, interval = c(min(profileLogLik$range), max(profileLogLik$range)),maximum = TRUE)
-    # MLE <- optimization$maximum 
-    # uniroot(f1, lower = 20000, upper = 80000)$root
-    # abline(v =ci[1], lty = 2)
-    # abline(v =ci[2], lty = 2)
-    # points(All, y = rep(0, length(All)), pch = 15, cex =0.7 )
-    # All2 <-c(afLeft(breaks95), afRight(breaks95))
-    # abline(v =All2[1], lty = 2)
-    # abline(v =All2[2], lty = 2)
-    # points(All2, y = rep(0, length(All2)), col = "red", cex = 0.7)
-    
-    
-    # leftOfMax = profileLogLik$range < MLE
-    # if(length(which(leftOfMax)) <=2 | length(which(!leftOfMax)) <=2){
-    #   ci95 = c(NA,NA)
-    #   print("Not enough data for CI calculation")
-    # }else{
-    #   afLeft <- approxfun(profileLogLik$profile[leftOfMax], profileLogLik$range[leftOfMax])   
-    #   afRight <- approxfun(profileLogLik$profile[!leftOfMax], profileLogLik$range[!leftOfMax])   
-    #   # plot(profileLogLik$profile[leftOfMax], profileLogLik$range[leftOfMax])
-    #   # plot(profileLogLik$profile[!leftOfMax], profileLogLik$range[!leftOfMax])
-    #   # curve(afLeft, add=TRUE)
-    #   # curve(afRight, add=TRUE)
-    #   
-    #  
-    #   ci95= c(afLeft(breaks95), afRight(breaks95))
-    #   if (any(is.na(ci95))){
-    #     warning("'range' scope too small for 95% ci")
-    #   }
-    # }
-
-  }
-  
-  
-  
-  
-  
-  if(is.element('shape',paramToEstimate)){
-    result = as.data.table(cbind(LogLikcpu, params0[,"shape"]))
-    colnames(result) <- c(paste(c('boxcox'), round(boxcox, digits = 2) ,sep = ''), "shape")
-    profileLogLik <- result[, .(profile=max(.SD)), by=shape]
-    # plot(profileLogLik$shape, profileLogLik$profile-breaks)
-    f1 <- approxfun(profileLogLik$shape, profileLogLik$profile-breaks)  
-    #f1 <- splinefun(profileLogLik$shape, profileLogLik$profile-breaks, method = "monoH.FC")
-    # curve(f1, add = TRUE, col = 2) 
-    # abline(h =0, lty = 2)
-    # shaperesults <- optim(0.1, f1, method = "L-BFGS-B",lower = 0.1, upper = 1.5, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(profileLogLik$shape)
-    upper = max(profileLogLik$shape)
-    MLE <- optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum
-    # maxvalue <- shaperesults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci for shape, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci for shape, require more params")}
-    }
-    
-    if(length(ci)==0 | length(ci)>2){
-      warning("require a better param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["shape",] <- c(MLE, ci)
-
-    # result = as.data.table(cbind(LogLikcpu, params0[,"shape"]))
-    # colnames(result) <- c(paste(c('boxcox'),boxcox,sep = ''), "shape")
-    # profileLogLik <- result[, .(profile=max(.SD)), by=shape]
-    # MLE <- profileLogLik$shape[which.max(profileLogLik$profile)]
-    # #plot(profileLogLik$shape, profileLogLik$profile)
-    # leftOfMax = profileLogLik$shape < profileLogLik$shape[which.max(profileLogLik$profile)]
-    # if(length(which(leftOfMax)) <=2 | length(which(!leftOfMax)) <=2){
-    #   ci95 = c(NA,NA)
-    #   print("Not enough data for CI calculation")
-    # }else{
-    #   afLeft <- approxfun(profileLogLik$profile[leftOfMax], profileLogLik$shape[leftOfMax])  
-    #   afRight <- approxfun(profileLogLik$profile[!leftOfMax], profileLogLik$shape[!leftOfMax])   
-    #   breaks95 = maximum - qchisq(0.95,  df = 1)/2
-    #   ci95= c(afLeft(breaks95), afRight(breaks95))
-    # }
-    # 
-    # 
-    # if (any(is.na(ci95)))
-    #   warning("'shape' scope too small for 95% ci")
-}       
-  
-  
-  
-  
-  
-  if(is.element('nugget',paramToEstimate)){
-    result = as.data.table(cbind(LogLikcpu, params0[,"nugget"]))
-    colnames(result) <- c(paste(c('boxcox'), round(boxcox, digits = 2) ,sep = ''), "nugget")
-    profileLogLik <-result[, .(profile=max(.SD)), by=nugget]
-    # plot(profileLogLik$nugget, profileLogLik$profile-breaks)
-    f1 <- approxfun(profileLogLik$nugget, profileLogLik$profile-breaks)  
-    # f1 <- splinefun(profileLogLik$nugget, profileLogLik$profile-breaks, method = "monoH.FC")
-    # curve(f1, add = TRUE, col = 2, n=1001) 
-    # abline(h =0, lty = 2)
-    # nuggetresults <- optim(0.1, f1, method = "L-BFGS-B",lower = 0.1, upper = 1.5, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(profileLogLik$nugget)
-    upper = max(profileLogLik$nugget)
-    MLE <-  optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum
-    # maxvalue <- nuggetresults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci for nugget, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci for nugget, require more params")}
-    }
-    
-    if(length(ci)==0 | length(ci)>2){
-      warning("require a better param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["nugget",] <- c(MLE, ci)
-    
-    # MLE <- profileLogLik$nugget[which.max(profileLogLik$profile)]
-    # leftOfMax = profileLogLik$nugget < MLE
-    # if(length(which(leftOfMax)) <=2 | length(which(!leftOfMax)) <=2){
-    #   ci95 = c(NA,NA)
-    #   print("Not enough data for CI calculation")
-    # }else{
-    #   afLeft <- approxfun(profileLogLik$profile[leftOfMax], profileLogLik$nugget[leftOfMax], rule=1)   # plot(profileLogLik$range, profileLogLik$profile)# curve(af, add=TRUE)
-    #   afRight <- approxfun(profileLogLik$profile[!leftOfMax], profileLogLik$nugget[!leftOfMax])   
-    #   # plot(profileLogLik$nugget, profileLogLik$profile)
-    #   # curve(afRight, add=TRUE)
-    #   ci95= c(afLeft(breaks95), afRight(breaks95))
-    # }
-    # if (any(is.na(ci95)))
-    #   warning("'nugget' scope too small for 95% ci")
-    
-  }
-  
-  
-  
-  
-  if(is.element('anisoRatio',paramToEstimate)){
-    result = as.data.table(cbind(LogLikcpu, params0[,"anisoRatio"]))
-    colnames(result) <- c(paste(c('boxcox'), round(boxcox, digits = 2) ,sep = ''), "anisoRatio")
-    profileLogLik <- result[, .(profile=max(.SD)), by=anisoRatio]
-    # plot(profileLogLik$anisoRatio, profileLogLik$profile-breaks)
-    f1 <- approxfun(profileLogLik$anisoRatio, profileLogLik$profile-breaks)  
-    # f1 <- splinefun(profileLogLik$anisoRatio, profileLogLik$profile-breaks, method = "monoH.FC")
-    # curve(f1, add = TRUE, col = 2, n=1001) 
-    # abline(h =0, lty = 2)
-    # anisoRatioresults <- optim(0.1, f1, method = "L-BFGS-B",lower = 0.1, upper = 1.5, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(profileLogLik$anisoRatio)
-    upper = max(profileLogLik$anisoRatio)
-    MLE <- round(optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum,digits=4)
-    # maxvalue <- anisoRatioresults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci for anisoRatio, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci for anisoRatio, require more params")}
-    }
-    
-    if(length(ci)==0 | length(ci)>2){
-      warning("require a better param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["anisoRatio",] <- c(MLE, ci)
-
-    # maximum <- max(profileLogLik$profile)
-    # MLE <- profileLogLik$anisoRatio[which.max(profileLogLik$profile)]
-    # 
-    # leftOfMax = profileLogLik$anisoRatio < MLE
-    # if(length(which(leftOfMax)) <=2 | length(which(!leftOfMax)) <=2){
-    #   ci95 = c(NA,NA)
-    #   print("Not enough data for CI calculation")
-    # }else{
-    #   afLeft <- approxfun(profileLogLik$profile[leftOfMax], profileLogLik$anisoRatio[leftOfMax])   
-    #   # plot(profileLogLik$anisoRatio, profileLogLik$profile)# curve(af, add=TRUE)
-    #   afRight <- approxfun(profileLogLik$profile[!leftOfMax], profileLogLik$anisoRatio[!leftOfMax])   
-    #   
-    #   ci95= c(afLeft(breaks95), afRight(breaks95))}
-    # if (any(is.na(ci95)))
-    #   warning("'anisoRatio' scope too small for 95% ci")
-
-  }
-  
-  
-  
-  if(is.element('anisoAngleDegrees',paramToEstimate)){
-    result = as.data.table(cbind(LogLikcpu, params0[,"anisoAngleDegrees"]))
-    colnames(result) <- c(paste(c('boxcox'), round(boxcox, digits = 2) ,sep = ''), "anisoAngleDegrees")
-    profileLogLik <- result[, .(profile=max(.SD)), by=anisoAngleDegrees]
-    #plot(profileLogLik$anisoAngleDegrees, profileLogLik$profile-breaks)
-    f1 <- approxfun(profileLogLik$anisoAngleDegrees, profileLogLik$profile-breaks)
-    # f1 <- splinefun(profileLogLik$anisoAngleDegrees, profileLogLik$profile-breaks, method = "monoH.FC")
-    # curve(f1, add = TRUE, col = 2, n=1001) 
-    # abline(h =0, lty = 2)
-    # anisoAngleDegreesresults <- optim(0.1, f1, method = "L-BFGS-B",lower = 0.1, upper = 1.5, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(profileLogLik$anisoAngleDegrees)
-    upper = max(profileLogLik$anisoAngleDegrees)
-    MLE <- round(optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum,digits=4)
-    # maxvalue <- anisoAngleDegreesresults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci, require more params")}
-    }
-    
-    if(length(ci)==0 | length(ci)>2){
-      warning("require a better param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["anisoAngleDegrees",] <- c(MLE, ci)
-
-    
-    # MLE <- profileLogLik$anisoAngleDegrees[which.max(profileLogLik$profile)]
-    # leftOfMax = profileLogLik$anisoAngleDegrees < MLE
-    # if(length(which(leftOfMax)) <=2 | length(which(!leftOfMax)) <=2){
-    #   ci95 = c(NA,NA)
-    #   print("Not enough data for CI calculation")
-    # }else{
-    #   afLeft <- approxfun(profileLogLik$profile[leftOfMax], profileLogLik$anisoAngleDegrees[leftOfMax])   
-    #   # plot(profileLogLik$anisoAngleDegrees, profileLogLik$profile)# curve(af, add=TRUE)
-    #   afRight <- approxfun(profileLogLik$profile[!leftOfMax], profileLogLik$anisoAngleDegrees[!leftOfMax])   
-    #   ci95= c(afLeft(breaks95), afRight(breaks95))}
-    # if (any(is.na(ci95)))
-    #   warning("'anisoAngleDegrees' scope too small for 95% ci")
-
-  }
-  
-  
-  
-  index <- which(LogLikcpu == max(LogLikcpu, na.rm = TRUE), arr.ind = TRUE)
-  
-  ###############lambda hat#####################
-  if(is.element('boxcox',paramToEstimate)  & length(boxcox)>3 ){
-    likForboxcox = cbind(boxcox, apply(LogLikcpu, 2, max) )
-    f1 <- approxfun(likForboxcox[,1], likForboxcox[,2]-breaks)
-    # f1 <- splinefun(likForboxcox[,1], likForboxcox[,2]-breaks, method = "monoH.FC")
-    # plot(likForboxcox[,1], likForboxcox[,2]-breaks)
-    # curve(f1(x), add = TRUE, col = 2, n = 1001)   #the number of x values at which to evaluate
-    # abline(h =0, lty = 2)
-    # rangeresults <- optim(26000, f1, method = "L-BFGS-B",lower = 20000, upper = 240000, hessian = FALSE, control=list(fnscale=-1) )
-    lower = min(boxcox)
-    upper = max(boxcox)
-    MLE <- optimize(f1, c(lower, upper), maximum = TRUE, tol = 0.0001)$maximum
-    # maxvalue <- rangeresults$objective
-    # breaks = maxvalue - qchisq(cilevel,  df = 1)/2
-    ci<-rootSolve::uniroot.all(f1, lower = lower, upper = upper)
-    if(length(ci)==1){
-      if( ci > MLE){
-        ci <- c(lower, ci)
-        warning("did not find lower ci for boxcox, require more params")
-      }else{
-        ci <- c(ci, upper)
-        warning("did not find upper ci for boxcox, require more params")}
-    }else if(length(ci)>2){
-      warning("error in param matrix")
-      ci <- c(NA, NA)
-    }
-    Table["boxcox",] <- c(MLE, ci)
-  }else{
-    Table["boxcox",1] <- boxcox[index[2]]
-  }
-  
+  # Table <- matrix(NA, nrow=length(union(paramToEstimate, 'boxcox'))+Ncov+1, ncol=3)
+  # rownames(Table) <-  c("intercept", paste(c('betahat'), seq_len(Ncov-1),sep = ''), "sdSpatial", union(paramToEstimate, 'boxcox'))
+  # colnames(Table) <-  c("estimate", paste(c('lower', 'upper'), cilevel*100, 'ci', sep = ''))
 
 
-  ###############betahat#####################
-  Betahat <- matrix(0, nrow=Ncov, ncol=Ndata)
-  a<-c( ((index[1]-1)*Ncov+1) : (index[1]*Ncov) )
-  mat <- XVYXVX[a,((Ndata+1):ncol(yx))]
-  mat[upper.tri(mat)] <- mat[lower.tri(mat)]
-  Betahat <- solve(mat) %*% XVYXVX[a,index[2]]
   
-  Table[c("intercept", paste(c('betahat'), seq_len(Ncov-1),sep = '')),1] <- Betahat
-  
-  
-  
-  
-  #################sigma hat#########################
-  if(reml==FALSE)  {
-    Table["sdSpatial",1] <- sqrt(ssqResidual[index[1],index[2]]/Nobs)
-  }else{         
-    Table["sdSpatial",1] <- sqrt(ssqResidual[index[1],index[2]]/(Nobs - Ncov))
-  }
+  # index <- which(LogLikcpu == max(LogLikcpu, na.rm = TRUE), arr.ind = TRUE)
+  # 
+  # 
+  # ###############betahat#####################
+  # Betahat <- matrix(0, nrow=Ncov, ncol=Ndata)
+  # a<-c( ((index[1]-1)*Ncov+1) : (index[1]*Ncov) )
+  # mat <- XVYXVX[a,((Ndata+1):ncol(yx))]
+  # mat[upper.tri(mat)] <- mat[lower.tri(mat)]
+  # Betahat <- solve(mat) %*% XVYXVX[a,index[2]]
+  # 
+  # Table[c("intercept", paste(c('betahat'), seq_len(Ncov-1),sep = '')),1] <- Betahat
+  # 
+  # 
+  # 
+  # 
+  # #################sigma hat#########################
+  # if(reml==FALSE)  {
+  #   Table["sdSpatial",1] <- sqrt(ssqResidual[index[1],index[2]]/Nobs)
+  # }else{         
+  #   Table["sdSpatial",1] <- sqrt(ssqResidual[index[1],index[2]]/(Nobs - Ncov))
+  # }
   
   
 
   Output <- list(LogLik=LogLikcpu,
                  minusTwoLogLik = minusTwoLogLik,
-                 estimates = Table,
+                 breaks = breaks,
                  Nobs = Nobs,
                  Ncov = Ncov,
                  Ndata = Ndata,
                  Nparam = Nparam,
-                 breaks = breaks,
                  ssqY=ssqY,     
                  ssqBetahat = ssqBetahat,
                  ssqResidual = ssqResidual,
