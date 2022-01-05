3d2cf71412e75b256c90f20603e821d59a81c4cf
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2022-01-05 15:10:51 +0000

    randomfields test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3092 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsp/tests/RFsimulate.R
+++ pkg/geostatsp/tests/RFsimulate.R
@@ -5,7 +5,9 @@ model <- c(var=5, range=20,shape=0.5)
 # any old crs
 theCrs = CRS("+proj=utm +zone=17 +datum=NAD27 +units=m +no_defs")
 
-if (requireNamespace("RandomFields", quietly = TRUE)) { 
+# don't test using the randomFields package, it's currently broken on some R builds
+options(useRandomFields = FALSE)
+if (requireNamespace("RandomFields", quietly = TRUE) & FALSE ) { 
   myraster = raster(nrows=50,ncols=50,xmn=100,ymn=100,xmx=110,ymx=110, 
     crs=theCrs)
 } else {
