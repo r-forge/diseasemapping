1b426958e8539d2e11513b7cafe5b50fbbd2e3c4
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2023-07-12 13:53:02 +0000

    north vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mapmiscTerra/tests/worldMap.R
@@ -0,0 +1,105 @@
+#+ setup
+
+library('mapmiscTerra')
+
+#if(!exists('mapmiscCachePath'))
+#  mapmiscCachePath = system.file('extdata', package='mapmisc')
+
+#if(!exists('mapmiscCacheReadOnly'))
+#  mapmiscCacheReadOnly = TRUE
+
+#mapmiscCachePath
+
+#options(
+#  mapmiscCachePath = mapmiscCachePath,
+#  mapmiscCacheReadOnly = mapmiscCacheReadOnly,
+#  mapmiscVerbose=TRUE)
+
+#getOption("mapmiscCachePath")
+#getOption("mapmiscCacheReadOnly")
+#'
+
+if(!interactive()) pdf("worldMap.pdf")
+#+ themaps, fig.cap='some maps', fig.subcap = c('projectable region', 'projected, with bg','projected, with world countries','projectable madagascar','madagascar')
+
+
+		data("worldMap")
+		worldMap = unwrap(worldMap)
+
+		world = project(worldMap, crsLL)
+		country='Japan'
+		x=world[grep(country, world$NAME)]
+
+		myCrsO = moll(x)
+
+ plot(world, ylim = 90.5*c(-1,1))
+ plot(attributes(myCrsO)$regionLL, 
+ 	border='red', col='#0000FF10', add=TRUE)
+
+
+
+ myMap = openmap(myCrsO, zoom=1, fact=0.7)
+ map.new(myMap)
+ plot(myMap, add=TRUE)
+ plot(attributes(myCrsO)$ellipse, add=TRUE, border='black')
+ gridlinesWrap(myCrsO, lty=2, col='orange')
+
+ xTcrop = wrapPoly(x=world, crs=myCrsO)
+
+	DcountryT  = grep(country, xTcrop$NAME)
+	
+	map.new(xTcrop, buffer=1000*1000)
+ plot(attributes(myCrsO)$ellipse, add=TRUE, col='lightBlue', border='blue')
+	plot(xTcrop,add=TRUE, col='grey')
+	plot(xTcrop[DcountryT,], col='red', lty=0, add=TRUE)
+	
+	gridlinesWrap(myCrsO, lty=2, col='orange')
+
+
+ country='Madagascar'
+	Dcountry  = grep(country, world$NAME)
+	x=world[Dcountry,]
+	
+	myCrsMoll = moll(x, flip= 'nwu' )
+	
+	plot(attributes(myCrsMoll)$ellipse)
+	
+	plot(world)
+	plot(attributes(myCrsMoll)$crop, border='red', col='#0000FF10', add=TRUE)
+	
+	xTcrop = wrapPoly(x=world, crs=myCrsMoll)
+	DcountryT  = grep(country, xTcrop$NAME)
+	
+	map.new(xTcrop)
+	plot(attributes(myCrsMoll)$ellipse, add=TRUE, col='lightBlue', border='blue')
+	plot(xTcrop,add=TRUE, col='grey')
+	plot(xTcrop[DcountryT,], col='green', add=TRUE)
+	
+	gridlinesWrap(crs=xTcrop, lty=2, col='red')
+
+
+	
+	country='Iceland'
+	Dcountry  = grep(country, world$NAME)
+	x=world[Dcountry,]
+	
+	myCrsMoll = moll(x,  flip= 'nwu' )
+	
+	plot(attributes(myCrsMoll)$ellipse)
+	
+	plot(world)
+	plot(attributes(myCrsMoll)$crop, border='red', col='#0000FF10', add=TRUE)
+	
+	xTcrop = wrapPoly(x=world, crs=myCrsMoll)
+	DcountryT  = grep(country, xTcrop$NAME)
+	
+	map.new(xTcrop)
+	plot(attributes(myCrsMoll)$ellipse, add=TRUE, col='lightBlue', border='blue')
+	plot(xTcrop,add=TRUE, col='grey')
+	
+	gridlinesWrap(crs=xTcrop, lty=2, col='red')
+	plot(xTcrop[DcountryT,], col='green', add=TRUE)
+	
+	if(!interactive()) dev.off()
+	
+#'
