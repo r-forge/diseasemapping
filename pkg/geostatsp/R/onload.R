aaa79d27d4e4b935d30f2e3c082239a767b3f00d
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2025-06-28 12:46:32 +0000

    Load
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@3544 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsp/R/onload.R
+++ pkg/geostatsp/R/onload.R
@@ -3,7 +3,7 @@
   is_check <- isTRUE(as.logical(Sys.getenv("_R_CHECK_PACKAGE_NAME_"))) ||
               isTRUE(as.logical(Sys.getenv("R_PACKAGE_CHECKING"))) ||
               any(grepl("CheckExEnv", search())) ||
-              !is.null(Sys.getenv("R_CMD_CHECK", unset = NULL))
+              !is.null(Sys.getenv("R_CMD_CHECK", unset = ""))
 
   if (is_check) {
     Sys.setenv(OMP_NUM_THREADS = "2")
